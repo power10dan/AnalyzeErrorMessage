a40031bbd0387b5b84c9dbc59a71717cadb88628
power10dan <lintzu@onid.oregonstate.edu>
2016-10-12 21:24:36 -0700

    My First Commit Of The Copy And Paste System

--- src/CompileAndAnalyzeMain.java
+++ src/CompileAndAnalyzeMain.java
@@ -2,6 +2,7 @@ import com.intellij.compiler.impl.CompileContextImpl;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.compiler.*;
 import com.intellij.openapi.project.Project;
 
@@ -13,39 +14,39 @@ public class CompileAndAnalyzeMain extends AnAction {
     public void actionPerformed(AnActionEvent e) {
 
         Project myProject = e.getData(PlatformDataKeys.PROJECT);
-        CompilationStatusListener myCompilationStatusListener = new CompilationStatusListener() {
-            // when compilation is finished,
-            // we want to get the error messages from the compileContext
-            // and analyze them.
-            @Override
-            public void compilationFinished(boolean aborted, int errors, int warnings, CompileContext compileContext) {
-                  CompilerMessage [] errorMessage = new CompilerMessage[errors];
-                  CompilerMessage [] warningMessages = new CompilerMessage[warnings];
-                  int status = 0;
-
-                  if(errors > 0) {
-                      errorMessage = compileContext.getMessages(CompilerMessageCategory.ERROR);
-
-                  }
-
-                  if(warnings > 0){
-                      warningMessages = compileContext.getMessages(CompilerMessageCategory.WARNING);
-                  }
-
-                  if(warnings == 0 || errors == 0){
-                      status = 1;
-                  }
-
-                  // get current time when finished...
-                  // we define project complete duration as
-                  // the entire time, including building time.
-                  final long actualProblemSolvingTime = System.currentTimeMillis();
-            }
-        };
 
         final CompilerManager myCompilerManager = CompilerManager.getInstance(myProject);
-        // add compilation listener to our project
-        myCompilerManager.addCompilationStatusListener(myCompilationStatusListener, myProject);
+        final long getCompilationStartTime = System.currentTimeMillis();
+
+        myCompilerManager.make(myCompilerManager.createProjectCompileScope(myProject), new CompileStatusNotification() {
+            @Override
+            public void finished(final boolean aborted, final int errors, final int warnings, final CompileContext compileContext) {
+                CompilerMessage [] errorMessage = new CompilerMessage[errors];
+                CompilerMessage [] warningMessages = new CompilerMessage[warnings];
+                int status = 0;
+
+                if(errors > 0) {
+                    errorMessage = compileContext.getMessages(CompilerMessageCategory.ERROR);
+                    System.out.println(errorMessage[0].getMessage());
+                }
+
+                if(warnings > 0){
+                    warningMessages = compileContext.getMessages(CompilerMessageCategory.WARNING);
+                }
+
+                if(warnings == 0 || errors == 0){
+                    status = 1;
+                }
+
+                // get current time when compilation is finished
+                final long compilationFinishTime = System.currentTimeMillis();
+                // actual time solving problem minus compilation time
+                final long timeDiff = compilationFinishTime - getCompilationStartTime;
+
+            }
+        });
+
+
 
     }
 }
