f4075ff02172caf0d88c101f102d51f7a1a1da5a
power10dan <lintzu@onid.oregonstate.edu>
2016-10-15 13:16:44 -0700

    Added Some Minor Changes

--- src/CompileAndAnalyzeMain.java
+++ src/CompileAndAnalyzeMain.java
@@ -1,10 +1,10 @@
-import com.intellij.compiler.impl.CompileContextImpl;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.compiler.*;
 import com.intellij.openapi.project.Project;
+
+import java.util.Random;
 
 /**
  * Created by Daniel Lin on 2016/10/6.
@@ -14,6 +14,12 @@ public class CompileAndAnalyzeMain extends AnAction {
     public void actionPerformed(AnActionEvent e) {
 
         Project myProject = e.getData(PlatformDataKeys.PROJECT);
+        // generate random session ID for each participant
+        // during each start of the program
+        int randomID = 0;
+        Random r = new Random( System.currentTimeMillis() );
+        randomID = ((1 + r.nextInt(2)) * 10000 + r.nextInt(10000));
+        final int randomIdentity = randomID;
 
         final CompilerManager myCompilerManager = CompilerManager.getInstance(myProject);
         final long getCompilationStartTime = System.currentTimeMillis();
@@ -23,6 +29,10 @@ public class CompileAndAnalyzeMain extends AnAction {
             public void finished(final boolean aborted, final int errors, final int warnings, final CompileContext compileContext) {
                 CompilerMessage [] errorMessage = new CompilerMessage[errors];
                 CompilerMessage [] warningMessages = new CompilerMessage[warnings];
+                // store number of errors, warnings, and the number of compilation attempts
+                // attempted so far
+
+                int [] compilationDetails = new int[3];
                 int status = 0;
 
                 if(errors > 0) {
@@ -42,11 +52,12 @@ public class CompileAndAnalyzeMain extends AnAction {
                 final long compilationFinishTime = System.currentTimeMillis();
                 // actual time solving problem minus compilation time
                 final long timeDiff = compilationFinishTime - getCompilationStartTime;
+                int randomIDFinished = 0;
+                randomIDFinished = randomIdentity;
+
 
             }
         });
-
-
 
     }
 }
